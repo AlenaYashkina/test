cb20a24571f3c598251ebd9c67a964fa991a856b
AlenaYashkina <delirium.tremens@inbox.ru>
2023-04-04 16:27:44 +0200

    Третий коммит в second ветке

--- com.test.kata/java/Main.java
+++ com.test.kata/java/Main.java
@@ -1,6 +1,6 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Второй коммит в другой ветке");
+        System.out.println("Третий коммит в second ветке");
     }
 }
